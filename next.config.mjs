1f3aa26182993b8db6b95aae152693d64b49d929
Cihad Paksoy <cihadp@gmail.com>
2025-04-13 16:44:00 +0300

    Add assetPrefix configuration and enhance webpack setup in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -7,9 +7,31 @@ export default (phase, { defaultConfig }) => {
   const nextConfig = {
     output: 'export',
     basePath: isDev ? '' : '/keyx-dialog-boilerplate',
-    assetPrefix: isDev ? undefined : '/keyx-dialog-boilerplate',
     images: {
       unoptimized: true,
+    },
+    webpack(config) {
+      config.module.rules.push({
+        test: /\.(css|scss)$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              url: {
+                filter: (url) => {
+                  if (url.startsWith('/')) {
+                    return `/keyx-dialog-boilerplate${url}`;
+                  }
+                  return url;
+                },
+              },
+            },
+          },
+          'sass-loader'
+        ],
+      });
+      return config;
     },
   };
 
