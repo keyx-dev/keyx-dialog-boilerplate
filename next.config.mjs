1656d956d8badf2bc57618807779ee94e80da8cb
Cihad Paksoy <cihadp@gmail.com>
2025-04-13 17:22:30 +0300

    Remove GitHub Actions deployment workflow and simplify next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,40 +1,5 @@
-import { PHASE_DEVELOPMENT_SERVER } from 'next/constants.js'
-
-
-export default (phase, { defaultConfig }) => {
-  const isDev = phase === PHASE_DEVELOPMENT_SERVER
-  
-  const nextConfig = {
-    output: 'export',
-    basePath: isDev ? '' : '/keyx-dialog-boilerplate',
-    images: {
-      unoptimized: true,
-    },
-    webpack(config) {
-      config.module.rules.push({
-        test: /\.(css|scss)$/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              url: {
-                filter: (url) => {
-                  if (url.startsWith('/')) {
-                    return `/keyx-dialog-boilerplate${url}`;
-                  }
-                  return url;
-                },
-              },
-            },
-          },
-          'sass-loader'
-        ],
-      });
-      return config;
-    },
-  };
-
-  return nextConfig;
+const nextConfig = {
+  output: 'export'
 }
 
+export default nextConfig
